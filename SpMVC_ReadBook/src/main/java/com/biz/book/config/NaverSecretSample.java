8bd53bf50b0bca214c2582b125d115819925724a
iceramqq <iceram22@naver.com>
2020-09-21 10:19:18 +0900

    readbook

---
+++ SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecretSample.java
@@ -0,0 +1,11 @@
+package com.biz.book.config;
+
+public class NaverSecretSample {
+/*
+ * 네이버 open api 등록을 수행한 후
+ * client
+ */
+	public final static String NAVER_CLIENT_ID = "YOUR NAVER CLIENT ID";
+	public final static String NAVER_CLIENT_SECRET = "YOUR NAVER SECRET";
+	
+}
